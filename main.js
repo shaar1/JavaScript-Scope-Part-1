aff915077e0c5d1705a27e1c70945edd45d1c680
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 14:23:04 +0100

    träning om scope som är global och local

---
+++ main.js
@@ -0,0 +1,71 @@
+/* global console, alert, prompt*/
+
+
+/*
+var myName = "Abdallah"; //Global Scope
+
+function myNameFunc(){
+
+"use strict"
+
+var myName = "Abdallah"
+}
+console.log(myName);
+*/
+
+
+/*
+var myName = "Abdallah"; //Global Scope
+
+function myNameFunc(){
+
+"use strict"
+
+var myName = "Anton"
+}
+console.log(myName);
+*/
+/*
+var myName = "Abdallah"; //Global Scope
+
+var myName = "Anton"
+
+function myNameFunc(){
+
+"use strict"
+
+}
+console.log(myName);
+*/
+
+/*
+var myName = "Abdallah"; //Global Scope
+
+var myName = "Anton"
+
+var myName = "Hassan"
+
+
+function myNameFunc(){
+
+"use strict"
+
+}
+console.log(myName);
+
+
+*/
+
+var myName = "Abdallah"; //Global Scope
+
+
+function myNameFunc(){
+
+"use strict"
+
+var myName = "Anton" // Local Scope
+
+}
+console.log(myName);
+
+
