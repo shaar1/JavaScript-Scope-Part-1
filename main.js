114e175184b44ad3d936a533a44e5c61da5b0428
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 14:47:47 +0100

    träning med olika exampel för console plats

--- main.js
+++ main.js
@@ -255,7 +255,7 @@ myNameFunc(); // 3 Local
 console.log(myName); // 4 
 */
 
-
+/*
 var myName = "Abdallah"; //Global Scope
 
 console.log(myName); // 1 Global
@@ -275,6 +275,75 @@ console.log(myName); //2 Global
 myNameFunc(); // 3 Local
 
 console.log(myName); // 4 
+*/
+/*
+var myName = "Abdallah"; //Global Scope
+
+console.log(myName); // 1 Global
+
+function myNameFunc() {
+
+    "use strict"
+
+    myName = "Anton" // Global Scope
+
+    console.log(myName);
+
+
+}
+console.log(myName); //2 Global
+
+myNameFunc(); // 3 Local
+
+console.log(myName); // 4 
+
+*/
+/*
+var myName = "Abdallah"; //Global Scope
+
+console.log(myName); // 1 Global
+
+function myNameFunc() {
+
+    "use strict"
+
+   var myName = "Anton" // Global Scope
+
+    console.log(myName);
+
+
+}
+console.log(myName); //2 Global
+
+myNameFunc(); // 3 Local
+
+console.log(myName); // 4 
+*/
+
+/*
+var myName = "Abdallah";
+
+console.log(myName);
+
+function myNameFunc() {
+
+    "use strict"
+
+   var myName = "Anton" 
+
+    console.log(myName);
+
+}
+
+myNameFunc();
+
+console.log(myName);
+
+console.log(myName);
+*/
+
+
+
 
 
 
